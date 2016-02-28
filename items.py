a18aa52d751c4c8758c766955bd12bcf7c44139a
Nikolaychik <nmateshko@gmail.com>
2016-02-28 17:53:55 +0200

    Fixed default items. Added item.tough

--- items.py
+++ items.py
@@ -11,6 +11,12 @@ class Item:
         self.name = item_name
 
 
+fist = Item
+fist.name = "Fist"
+fist.attack = 1
+fist.defense = 1
+fist.tough = 1000000000000000000
+
 class Weapon(Item):
     pass
 
@@ -22,10 +28,26 @@ class Armor(Item):
 class BootsArmor(Armor):
     pass
 
+naked_boots = BootsArmor
+naked_boots.name = "Naked"
+naked_boots.attack = 1
+naked_boots.defense = 1
+naked_boots.tough = 1000000000000000000
 
 class HeadArmor(Armor):
     pass
 
+head = HeadArmor
+head.name = "Head"
+head.attack = 1
+head.defense = 1
+head.tough = 1000000000000000000
 
 class BodyArmor(Armor):
     pass
+
+naked_body = BodyArmor
+naked_body.name = "Naked"
+naked_body.attack = 1
+naked_body.defense = 1
+naked_body.tough = 1000000000000000000
--- test.py
+++ test.py
@@ -1,15 +1,8 @@
-from units import Knight, Enemy
+import time
+class Math:
+    x = 0
+    @property
+    def func(self):
+        self.x += 5
 
-hero = Knight("Dude")
-orc1 = Enemy("ORC")
-ogr = Enemy("OGR")
-hero.battle_begin([orc1, ogr])
-
-count = 0
-for value in hero.enemies:
-    print "[%s] %s" % (count, value.name)
-    count += 1
-enemy_id = raw_input("Enter enemy ID: ")
-enemy = hero.enemies[int(enemy_id)]
-
-print enemy.name
+while True:
