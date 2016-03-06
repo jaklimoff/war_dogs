5767a675e7d7535e29f9d4d2e7c0fc7d4e690aba
skypro1111 <skypro1111@gmail.com>
2016-03-06 02:54:26 +0200

    please, call me Sky

--- fighters/skypro1111.py
+++ fighters/skypro1111.py
@@ -1,19 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = 'skypro1111'
+
 from final_battle import Unit
-import random
-__author__ = 'Pro'
+
 
 
 class Skypro(Unit):
     name = 'skypro1111'
+    dmg_coef = 1.5
     def update(self):
         e = filter(lambda enemy: (enemy.is_alive and enemy.name != self.name), self.enemies)
-        mn = min([enm.hp for enm in e])
+        min_hp_tup = (enm.hp for enm in e)
+        if min_hp_tup:
+            mn = min(min_hp_tup)
+        else:
+            mn = 0
+
         for enm in self.enemies:
             if enm.hp == mn:
                 enemy = enm
-        if self.hp <= sum([(7.5 + 7.5*enm.st/100) for enm in e])*1.5:
+        if self.hp <= sum([(7.5 + 7.5*enm.st/100) for enm in e])*self.dmg_coef:
             getattr(self, "_heal")(self)
-            # print "heal"
         else:
             getattr(self, "_hit")(enemy)
-            # print "hit"
