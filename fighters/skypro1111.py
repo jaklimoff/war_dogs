c964488829913333dd7a1f674da6248dd3d9f5d3
skypro1111 <skypro1111@gmail.com>
2016-03-13 17:43:29 +0200

    final version

--- fighters/skypro1111.py
+++ fighters/skypro1111.py
@@ -2,8 +2,7 @@
 # -*- coding: utf-8 -*-
 __author__ = 'skypro1111'
 
-from final_battle import Unit
-import random
+from final_battle import Unit, Environment
 
 class Skypro(Unit):
     dmg_coef = 1
@@ -12,10 +11,68 @@ class Skypro(Unit):
         Unit.__init__(self)
         self.name = name
         self.filtered_enemies = []
+        self.nearest_enemies = []
 
-    def filter_enemies(self, enemies):
-        tmp = filter(lambda enemy: (enemy.is_alive and enemy.name != self.name), enemies)
-        return filter(lambda enemy: (abs(abs(enemy.position[0])-abs(self.position[0]))<2 and abs(abs(enemy.position[1])-abs(self.position[1]))<2), tmp)
+    def goto(self, enemies):
+        enemy = self.choose_enemy(enemies)
+        if enemy != None:
+            if self.hp <= 30:
+                if abs(self.position[0] - enemy.position[0]) > 1:
+                    if self.position[0] > enemy.position[0]:
+                        x1 = self.position[0]+1
+                    else:
+                        x1 = self.position[0]-1
+                else:
+                    x1 = self.position[0]
+
+                if abs(self.position[1] - enemy.position[1]) > 1:
+                    if self.position[1] > enemy.position[1]:
+                        y1 = self.position[1]+1
+                    else:
+                        y1 = self.position[1]-1
+                else:
+                    y1 = self.position[1]
+
+            else:
+                if abs(self.position[0] - enemy.position[0]) > 1:
+                    if self.position[0] > enemy.position[0]:
+                        x1 = self.position[0]-1
+                    else:
+                        x1 = self.position[0]+1
+                else:
+                    x1 = self.position[0]
+
+                if abs(self.position[1] - enemy.position[1]) > 1:
+                    if self.position[1] > enemy.position[1]:
+                        y1 = self.position[1]-1
+                    else:
+                        y1 = self.position[1]+1
+                else:
+                    y1 = self.position[1]
+
+        self._move(y1, x1)
+
+
+    def action(self, enemies):
+        enemy = self.choose_enemy(enemies)
+        print enemy.name
+        if self.st >= 50:
+            self._bighit(enemy)
+            return
+        elif self.hp <= sum([(7.5 + 7.5*enm.st/100) for enm in enemies])*self.dmg_coef and self.mp >= 10:
+            self._heal(self)
+            return
+        else:
+            self._hit(enemy)
+            return
+
+    def get_all_enemies(self, enemies):
+        return filter(lambda enemy: (enemy.is_alive and enemy != self), enemies)
+
+    def get_nearest_enemies(self, enemies):
+        return filter(lambda enemy: (abs(abs(enemy.position[0])-abs(self.position[0]))<2
+                                     and abs(abs(enemy.position[1])-abs(self.position[1]))<2
+        ), enemies)
 
     def choose_enemy(self, enemies):
         if enemies != []:
@@ -27,23 +84,18 @@ class Skypro(Unit):
         else:
             mn = 0
         for enm in enemies:
-            if enm.hp == mn:
+            if enm.name == 'BigDaddy':
+                return enm
+            elif enm.hp == mn:
                 return enm
 
     def update(self):
-        self.filtered_enemies = self.filter_enemies(self.enemies)
-        enemy = self.choose_enemy(self.filtered_enemies)
+        self.filtered_enemies = self.get_all_enemies(self.enemies)
+        self.nearest_enemies = self.get_nearest_enemies(self.filtered_enemies)
 
-        if self.st >= 50:
-            getattr(self, "_bighit")(enemy)
-            return
-
-        if self.filtered_enemies == []:
-            self._move(random.randint(-1, 1), random.randint(-1, 1))
-            return
-        if self.hp <= sum([(7.5 + 7.5*enm.st/100) for enm in self.filtered_enemies])*self.dmg_coef:
-            getattr(self, "_heal")(self)
-            return
+        if self.nearest_enemies == [] or self.hp <= 50:
+            self.goto(self.filtered_enemies)
+            print 'goto'
         else:
-            getattr(self, "_hit")(enemy)
-            return
+            self.action(self.nearest_enemies)
+            print 'action'
